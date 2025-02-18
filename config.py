acdba0a0ed5c715bb50c29c6c068c005bece7538
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-02-18 19:05:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ API_ID = int(environ.get("API_ID", "27152769"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "7650392649:AAHPpHvaHsjFtl27-q_ImkE1ZeHsGkls8kE")
 BOT_OWNER = int(environ.get("BOT_OWNER", "5756495153"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Feedbackdenabot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-4601190800"))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002298830187"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002298830187"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-4601190800"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://user1:abhinai.2244@cluster0.7oaqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Define default emojis list
