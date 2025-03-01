a58539f98565137e987f132aec5da7b77d6df75a
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-03-01 10:36:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "b98dff566803b43b3c3120eec537fc1d")
-API_ID = int(environ.get("API_ID", "27152769"))
+API_HASH = environ.get("API_HASH", "e35a05d338376cbcd8162f810aed878d")
+API_ID = int(environ.get("API_ID", "29483517"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "7650392649:AAHPpHvaHsjFtl27-q_ImkE1ZeHsGkls8kE")
 BOT_OWNER = int(environ.get("BOT_OWNER", "5756495153"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Feedbackdenabot")
