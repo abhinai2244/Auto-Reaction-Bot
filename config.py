14be8c3769f3ef69d43e1846b1358a0321679223
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-03-13 16:43:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(environ.get("API_ID", "29483517"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "7650392649:AAHPpHvaHsjFtl27-q_ImkE1ZeHsGkls8kE")
 BOT_OWNER = int(environ.get("BOT_OWNER", "5756495153"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Feedbackdenabot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-4627777958"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002497873969"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002497873969"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://user1:abhinai.2244@cluster0.7oaqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
